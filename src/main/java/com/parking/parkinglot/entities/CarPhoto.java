4c3c921fb899070d49653dd3ee613b1c706e26f0
andreisas01 <andreisas01@yahoo.com>
2025-12-15 09:21:45 +0200

    lab10: added file upload

--- src/main/java/com/parking/parkinglot/entities/CarPhoto.java
+++ src/main/java/com/parking/parkinglot/entities/CarPhoto.java
@@ -1,7 +1,66 @@
 package com.parking.parkinglot.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "car_photo")
 public class CarPhoto {
-  }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "filename", nullable = false)
+    private String filename;
+
+    @OneToOne(mappedBy = "photo", cascade = CascadeType.ALL, optional = false, orphanRemoval = true)
+    private Car car;
+
+    @Lob
+    @Column(name = "file_content", nullable = false)
+    private byte[] fileContent;
+
+    @Column(name = "file_type", nullable = false)
+    private String fileType;
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public byte[] getFileContent() {
+        return fileContent;
+    }
+
+    public void setFileContent(byte[] fileContent) {
+        this.fileContent = fileContent;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
